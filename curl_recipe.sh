e0a8cef6d0e97ebdac79cbe2b7e00dfe083f83d7
eyerene14 <eyerene14@hotmail.com>
2026-07-06 12:15:56 -0500

    revised curl_recipe.sh

--- curl_recipe.sh
+++ curl_recipe.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 
 url=${1:?"Usage: $0 <url> <filename>"}
+temp_file=$(mktemp -p /tmp)
 filename=${2:?"Usage: $0 <url> <filename>"}
+recipe_path="$HOME/Documents/icloudDocs/Recipe_backups"
+
+cd "$recipe_path"
+
+trap "rm -f $temp_file" 0 2 3 15
+echo 'ls /tmp/'
+ls /tmp/*
+#tail -f $temp_file > /dev/null &
+
+curl $url -i  \
+-H 'x-requested-with: XMLHttpRequest' \
+--compressed -o $filename | sed '1,/^$/d'
 
 # if the filename ends in html strip it out
 sed 's/\.html$//' <<< "$filename" > /dev/null || filename="$filename.html"
 
-curl $url -i  \
--H 'x-requested-with: XMLHttpRequest' \
---compressed -o /Users/ijaramil/Library/Mobile\ Documents/com\~apple\~CloudDocs/Documents/Recipe_backups/$filename | sed '1,/^$/d'
+cat $filename > $temp_file
 
+perl -e 's#<div data-testid="ContentFooterBottom" class="ContentFooterBottom-cbbwZo iSuBdg">.*?</div>(?=\s*</main>)##s' $temp_file && mv /tmp/tmp.* "$filename".html
 
-/Applications/Safari.app/Contents/MacOS/Safari -bna 'file:///Users/ijaramil/Library/Mobile\ Documents/com\~apple\~CloudDocs/Documents/Recipe_backups/$filename'
+open .
+
+/Applications/Safari.app/Contents/MacOS/Safari -bna "file:///$filename"
